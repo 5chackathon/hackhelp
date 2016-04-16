1c5447a37d2d05f6fbc459af482a1388b3e00728
rwoll <ross.wollman@gmail.com>
2016-04-15 18:52:11 -0700

    init commit

---
+++ app.jsx
@@ -0,0 +1,634 @@
+var data = {
+  'Collaboration Tools': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://github.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'GitHub'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://try.github.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'Try Git'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://git-scm.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Git Docs'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://slack.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Slack'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://bitbucket.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'BitBucket'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.atlassian.com/git/',
+        'subtitle': 'No subtitle.',
+        'title': 'Git Guide by Atlassin'
+      }
+    ],
+    'description': 'A selection of tools to help you work better with your team. Collaborate. Code. Create.'
+  },
+  'Databases and Services': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://www.firebase.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Firebase'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.sqlite.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'SQLite'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://mlab.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Mongo Lab'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.mysql.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'MySQL'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.mongodb.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'MongoDB'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://www.postgresql.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'PostgreSQL'
+      }
+    ],
+    'description': 'Need someway of having your application remember data? Check out these solutions.'
+  },
+  'Deployment and Hosting': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://pages.github.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'GitHub Pages'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.heroku.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Heroku'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://aws.amazon.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Amazon Web Services (AWS)'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.digitalocean.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Digital Ocean'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://bitnami.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Bitnami'
+      }
+    ],
+    'description': 'Show your work to the world!'
+  },
+  'Hardware': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://www.arduino.cc/',
+        'subtitle': 'No subtitle.',
+        'title': 'Arduino'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.arduino.cc/en/Main/Software',
+        'subtitle': 'No subtitle.',
+        'title': 'Arduino Software'
+      }
+    ],
+    'description': 'Who doesn\'t love hardware?'
+  },
+  'Integrated Development Environments (IDE) and Editors': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://jsfiddle.net/',
+        'subtitle': 'No subtitle.',
+        'title': 'JSFiddle'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://codepen.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'CodePen'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://c9.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'Cloud 9'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://atom.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'Atom'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://atom.io/packages',
+        'subtitle': 'No subtitle.',
+        'title': 'Atom Packages'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://atom.io/packages/firepad',
+        'subtitle': 'No subtitle.',
+        'title': 'Firepad: Realtime Coding Package'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.sublimetext.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Sublime'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://eclipse.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Eclipse for Java Development'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.jetbrains.com/idea/',
+        'subtitle': 'No subtitle.',
+        'title': 'IntelliJ IDEA for Java Development'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.jetbrains.com/pycharm/',
+        'subtitle': 'No subtitle.',
+        'title': 'PyCharm CE for Python Development'
+      }
+    ],
+    'description': 'Write code easily with these tools!'
+  },
+  'Miscellaneous': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'http://brew.sh/',
+        'subtitle': 'No subtitle.',
+        'title': 'Homebrew Package Manager for Mac'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.apple.com/resources/',
+        'subtitle': 'No subtitle.',
+        'title': 'Apple Developer Resources'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://developer.android.com/index.html',
+        'subtitle': 'No subtitle.',
+        'title': 'Android Developer Resources'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://msdn.microsoft.com/en-us/dn308572.aspx',
+        'subtitle': 'No subtitle.',
+        'title': 'Microsoft Developer Resources'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://scratch.mit.edu/',
+        'subtitle': 'No subtitle.',
+        'title': 'Scratch Visual Programming by MIT'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://www.json.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'JSON Data Specification'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://emojione.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'EmojiOne'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://octicons.github.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Octicons'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://oauth.net/',
+        'subtitle': 'No subtitle.',
+        'title': 'OAuth'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.python.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Python Docs and Downloads'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.haskell.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Haskell Docs and Downloads'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://processing.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Processing Docs'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://py.processing.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Python Processing'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://curl.haxx.se/docs/manpage.html',
+        'subtitle': 'No subtitle.',
+        'title': 'cURL Command Line Docs'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://virtualenv.pypa.io/en/latest/',
+        'subtitle': 'No subtitle.',
+        'title': 'Python Virtual Environments'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://cordova.apache.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Cordova'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://phonegap.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'PhoneGap'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://textblob.readthedocs.org/en/dev/',
+        'subtitle': 'No subtitle.',
+        'title': 'Python Text Processing Package'
+      }
+    ],
+    'description': 'A collection of other cool projects and packages.'
+  },
+  'Web APIs': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://www.yelp.com/developers',
+        'subtitle': 'No subtitle.',
+        'title': 'Yelp'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developers.google.com/maps/',
+        'subtitle': 'No subtitle.',
+        'title': 'Google Maps'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.spotify.com/web-api/',
+        'subtitle': 'No subtitle.',
+        'title': 'Spotify'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.twilio.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Twilio'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://dev.twitter.com/overview/documentation',
+        'subtitle': 'No subtitle.',
+        'title': 'Twitter'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developers.facebook.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Facebook'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.mailgun.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Mailgun'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://developer.nytimes.com/docs',
+        'subtitle': 'No subtitle.',
+        'title': 'NYTimes'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.braintreepayments.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Braintree Payments'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://stripe.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Stripe'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://orchestrate.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'Orchestrate'
+      }
+    ],
+    'description': 'This list of APIs features well-written and well-documented web APIs to get you hacking faster with other services.'
+  },
+  'Web-Development': {
+    'data': [
+      {
+        'description': 'No description.',
+        'link': 'https://developer.mozilla.org/en-US/',
+        'subtitle': 'No subtitle.',
+        'title': 'Mozilla Developer Network (MDN)'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+        'subtitle': 'No subtitle.',
+        'title': 'HTML Guides from MDN'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+        'subtitle': 'No subtitle.',
+        'title': 'CSS Guides from MDN'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://sass-lang.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Sass'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://lesscss.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Less'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        'subtitle': 'No subtitle.',
+        'title': 'JavaScript Guides from MDN'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.chrome.com/devtools',
+        'subtitle': 'No subtitle.',
+        'title': 'Chrome Dev Tools'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://jquery.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'jQuery Documentation'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://semantic-ui.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Semantic UI'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://getbootstrap.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Bootstrap'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://materializecss.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Materialize CSS'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://d3js.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'D3.js'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://p5js.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'p5.js'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://goratchet.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Ratchet'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://nodejs.org/en/',
+        'subtitle': 'No subtitle.',
+        'title': 'Node.js'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.npmjs.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'npm: Node Package Manager'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://expressjs.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Express'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://passportjs.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Passport'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://mozilla.github.io/nunjucks/',
+        'subtitle': 'No subtitle.',
+        'title': 'Nunjucks'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://socket.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'Socket.IO'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://browserify.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Browserify'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://flask.pocoo.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Flask'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.djangoproject.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Django'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://facebook.github.io/react/',
+        'subtitle': 'No subtitle.',
+        'title': 'React UI'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://angularjs.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'AngularJS'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://rubyonrails.org/',
+        'subtitle': 'No subtitle.',
+        'title': 'Ruby on Rails'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://www.meteor.com/',
+        'subtitle': 'No subtitle.',
+        'title': 'Metor'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.chrome.com/extensions/devguide',
+        'subtitle': 'No subtitle.',
+        'title': 'Chrome Extensions Dev Guide'
+      },
+      {
+        'description': 'No description.',
+        'link': 'https://developer.chrome.com/apps/first_app',
+        'subtitle': 'No subtitle.',
+        'title': 'Chrome Applications Guide'
+      },
+      {
+        'description': 'No description.',
+        'link': 'http://electron.atom.io/',
+        'subtitle': 'No subtitle.',
+        'title': 'Electron Application Guide'
+      }
+    ],
+    'description': 'From creating a one page web application to the next big application, these frameworks and tools will help you get started!'
+  }
+};
+
+var ResourceCard = React.createClass({
+  render: function () {
+    return (
+    <div className="ui card">
+      <div className="content">
+        <div className="header">
+          {this.props.data.title}
+        </div>
+        {/*
+        <div className="meta">
+          {this.props.data.subtitle}
+        </div>
+        <div className="description">
+          {this.props.data.description}
+        </div>
+        */}
+      </div>
+      <div className="extra content">
+        <a href={ this.props.data.link}>Visit Site</a>
+      </div>
+    </div>
+    );
+  }
+});
+
+var CategoryBlock = React.createClass({
+  render: function() {
+    return (
+      <div className="ui vertical segment">
+        <h2 className="ui center aligned header">
+          { this.props.category }
+          <div className="sub header">{this.props.description}</div>
+        </h2>
+        <div className="ui centered link cards">
+          {this.props.list.map(function(entry) {
+            return (<ResourceCard data={entry} />);
+          })}
+        </div>
+      </div>
+    )}
+});
+
+var CategoriesBlock = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <CategoryBlock description={this.props.data["Collaboration Tools"].description} category={"Collaboration Tools"} list={this.props.data['Collaboration Tools']["data"]} />
+        <CategoryBlock description={this.props.data["Integrated Development Environments (IDE) and Editors"].description} category={"Integrated Development Environments (IDE) and Editors"} list={this.props.data['Integrated Development Environments (IDE) and Editors']["data"]} />
+        <CategoryBlock description={this.props.data["Web-Development"].description} category={"Web Development"} list={this.props.data['Web-Development']["data"]} />
+        <CategoryBlock description={this.props.data["Web APIs"].description} category={"Web APIs"} list={this.props.data['Web APIs']["data"]} />
+        <CategoryBlock description={this.props.data["Databases and Services"].description} category={"Databases and Services"} list={this.props.data['Databases and Services']["data"]} />
+        <CategoryBlock description={this.props.data["Deployment and Hosting"].description} category={"Deployment and Hosting"} list={this.props.data['Deployment and Hosting']["data"]} />
+        <CategoryBlock description={this.props.data["Miscellaneous"].description} category={"Miscellaneous"} list={this.props.data['Miscellaneous']["data"]} />
+        <CategoryBlock description={this.props.data["Hardware"].description} category={"Hardware"} list={this.props.data['Hardware']["data"]} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CategoriesBlock data={data} />,
+  document.getElementById('main-content')
+);
